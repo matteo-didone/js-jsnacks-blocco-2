2882f19a8d20db0ce75a525dacb1da8de6ac6dde
matteo-didone <matteo.dido9@gmail.com>
2023-05-19 16:46:07 +0200

    First attempt at solving exercise 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,13 +1,25 @@
-//Ask the user to insert a number for 10 times, and sum the numbers provided
+//Create an empty array
+let numbersArray = [];
+
+//Create a variable where the sum will be stored 
 let sum = 0;
 
-for (let i = 0; i < 10; i++) 
+// Ask the user to insert a number until the sum of all numbers is less than 50
+while(sum < 50) 
 {
+    // Ask the user to insert a number
     let number = parseInt(prompt("Insert a number: "));
+    // Add the number to the array
+    numbersArray.push(number);
+    // Add the number to the sum
     sum += number;
-    console.log(sum);
+    // Print the sum in the console
+    console.log("The sum so far is: " + sum);
 
-    
+    if(sum >= 50)
+    {
+        break;
+    }
 }
 
 // Now, I print the final result in the HTML page
