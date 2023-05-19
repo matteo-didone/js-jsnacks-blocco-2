e9458ffc725d382913032c1eff3d71ffc22a1d30
matteo-didone <matteo.dido9@gmail.com>
2023-05-19 17:34:07 +0200

    Finished first attempt at second snack

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,89 +1,53 @@
-//I need to ask the guest for their name, check if their name is on the guest list, and print a message appropriate on the result of the check
-const guestName = prompt("Please provide your name: ");
-console.log("Guest Name: " + guestName);
-var guestFound = false;
-
-//Create an array of email addresses
-const guestList = 
-[
-    "Jay Gatsby",
-    "Daisy Buchanan",
-    "Tom Buchanan",
-    "Nick Carraway",
-    "Jordan Baker",
-    "Myrtle Wilson", 
-    "George Wilson",
-    "Owl Eyes",
-    "Meyer Wolfsheim",
-    "Klipspringer",
-    "Catherine",
-    "Henry C. Gatz",
-    "Ewing Klipspringer",
-    "Dan Cody", 
-    "Chester McKee",
-    "Lucille McKee",
-    "Jayne Knope",
-    "Grace Vanderbilt",
-    "Patrick Callahan",
-    "Amelia Astor",
-    "Theodore Sinclair",
-    "Penelope Harper",
-    "Alexander Vanderbilt",
-    "Isabella Preston",
-    "Vivian Devereaux",
-    "Benjamin Kingsley",
-    "Olivia St. Clair",
-    "Maximilian Rothschild",
-    "Scarlett Worthington",
-    "Sebastian Ashford",
-    "Genevieve Montague",
-    "Christopher Harrington",
-    "Arabella Kensington",
-    "Wesley Chambers",
-    "Seraphina Fairfax",
-    "Nathaniel Hartington",
-    "Victoria Sinclair",
-    "Christian Davenport",
-    "Juliana Huntington",
-    "Harrison Beaumont",
-];
-
-// Create the <p> elements for name provided and guest list presence
-const guestNameParagragh = document.createElement('p');
-guestName.id = 'guest-name';
-const guestListPresence = document.createElement('p');
-guestListPresence.id = 'guest-list-presence';
-
-// Get the guest-list-container div
-const guestListContainer = document.querySelector('.guest-list-container');
-
-// Append the <p> elements to the container
-guestListContainer.appendChild(guestNameParagragh);
-guestListContainer.appendChild(guestListPresence);
-
-//Display the guest's name in the document
-guestNameParagragh.innerHTML = "Guest Name: " + guestName;
+//Generate a random number between 1 and 100 included
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+//Math.random() generates a random number between 0 and 1 (excluded)
+//Math.floor() rounds the number down to the nearest integer
+//Times 100 to get a number between 0 and 100 (excluded)
+//Plus 1 to get a number between 1 and 101 (excluded)
 
 
-// Check if the guest name is on the guest list
-for (let i = 0; i < guestList.length && !guestFound; i++) 
+//Create a variable to store the user's guess number
+let userGuess = 0;
+//Create a variable to store the number of guesses
+let numberOfGuesses = 0;
+//Create a variable to store whether the user has guessed the number correctly
+let correctGuess = false;
+
+//Keep asking the user to guess a number, up until the user exactly guesses the correct number
+while(userGuess === randomNumber && correctGuess === false)
 {
-    if (guestList[i] === guestName) 
+    userGuess = parseInt(prompt("Guess a number between 1 and 100: "));
+    numberOfGuesses++;
+
+    if(userGuess === randomNumber)
     {
-        guestFound = true;
+        console.log("Congratulations, you guessed the correct number!");
+        console.log("Number of guesses: " + numberOfGuesses);
+        correctGuess = true;
+    }
+    else if(userGuess < randomNumber)
+    {
+        console.log("Your guess is too low, try again");
+    }
+    else if(userGuess > randomNumber)
+    {
+        console.log("Your guess is too high, try again");
     }
 }
 
-// Determine the status and display appropriate message
-if (guestFound === true) 
-{
-    const accessGranted = "Welcome to the party!";
-    console.log(accessGranted);
-    guestListPresence.innerHTML = accessGranted;
-} 
-else if (guestFound === false)
-{
-    const accessDenied = "I'm sorry, but your name doesn't appear in the guest list";
-    console.log(accessDenied);
-    guestListPresence.innerHTML = accessDenied;
-}
+// Create the <p> elements for name provided and guest list presence
+const userGuessParagragh = document.createElement('p');
+userGuessParagragh.id = 'user-guess';
+const numberOfGuessesParagraph = document.createElement('p');
+numberOfGuessesParagraph.id = 'number-of-guesses';
+
+// Get the container div
+const guessesContainer = document.querySelector('.guesses-container');
+
+// Append the <p> elements to the container
+guessesContainer.appendChild(userGuessParagragh);
+guessesContainer.appendChild(numberOfGuessesParagraph);
+
+//Display the guest's name in the document
+userGuessParagragh.innerHTML = "Your guess: " + userGuess;
+numberOfGuessesParagraph.innerHTML = "Number of guesses: " + numberOfGuesses;
