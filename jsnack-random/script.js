4d41b9668f85b56dd6976ab17ede1c594cdfc761
matteo-didone <matteo.dido9@gmail.com>
2023-05-24 16:28:02 +0200

    Added necessary checks

--- jsnack-random/script.js
+++ jsnack-random/script.js
@@ -1,58 +1,87 @@
-// Given an interval of numbers between 1 and 5 (included), 
-// generate a set of four random numbers that are different from each other, 
-// and print such set to the console. 
+// I declare the variables that will store the values provided by the user
+let rangeStart;
+let rangeEnd;
+let numberElements;
 
-// Generate a random number between 1 and 5 included
-const randomNumber = generateRandomNumber(1, 5);
-// Math.random() generates a random number between 0 and 1 (excluded)
-// Math.floor() rounds the number down to the nearest integer
-// Times 5 to get a number between 0 and 5 (excluded)
-// Plus 1 to get a number between 1 and 5 (included)
+// I need to make sure that:
+// 1) The values provided by the user are numbers
+// 2) The rangeStart is smaller than the rangeEnd
+// 3) The numberElements is smaller than the rangeEnd - rangeStart + 1 (because the range includes the rangeStart and the rangeEnd)
 
-// Generate a set of three random numbers that are different from each other
-const setOfFour = set(); //Call the function set()
+// Check if the values provided by the user are numbers
+while
+(
+    isNaN(rangeStart) || 
+    isNaN(rangeEnd) || 
+    isNaN(numberElements) || 
+    rangeStart === "" || 
+    rangeEnd === "" || 
+    numberElements === "" ||
+    rangeStart === null ||
+    rangeEnd === null ||
+    numberElements === null ||
+    rangeStart > rangeEnd || 
+    (rangeEnd - rangeStart + 1) < numberElements
+)    
+{
+    // Ask the user for the range of numbers and the number of elements in the set
+    rangeStart = prompt("What's the number from which you'd want the range to start?"); // The first number in the range
+    rangeEnd = prompt("What's the number from which you'd want the range to end?"); // The last number in the range
+    numberElements = prompt("How many elements would you like to have in the new array?"); // The number of elements in the set
+}
+
+// Generate a set of random numbers that are different from each other
+const setOfNumbers = set(rangeStart, rangeEnd, numberElements); // Call the function set()
 
 // Print the random set to the console
-console.log(setOfFour);
-
+console.log(setOfNumbers);
 
 // Print the set to the HTML page
 const setElement = document.getElementById("set");
-setElement.innerHTML = setOfFour;
+setElement.innerHTML = setOfNumbers;
 
-// Function to generate a set of four random numbers that are different from each other
-function set () 
+// Function to generate a set of random numbers that are different from each other
+function set(rangeStart, rangeEnd, numberElements) 
 {
-    const beginningSet = ['1','2','3','4','5']; // Create an array with the numbers 1 to 5
-    const randomSet = []; // Create a new empty array to store the random numbers
+    const beginningSet = [];
 
-    // While the length of the new array is less than 4
-    while (randomSet.length < 4) 
+    // Populate the array with numbers from rangeStart to rangeEnd
+    for (let i = rangeStart; i <= rangeEnd; i++) 
     {
-        // Generate a random index
+        beginningSet.push(i);
+    }
+
+    // Create an array to add the set of random numbers that are different from each other
+    const randomSet = [];
+
+    // Generate a set of random numbers that are different from each other and add them to the array
+    while (randomSet.length < numberElements) 
+    {
+        // Find a random number from the beginningSet (by using a random index) array and add it to the randomSet array
         const randomIndex = Math.floor(Math.random() * beginningSet.length);
-        // Get the element at the random index
         const randomElement = beginningSet[randomIndex];
 
-        // Check if the randomElement is already present in randomSet
+        // Check if the random number is already in the randomSet array
         if (!randomSet.includes(randomElement)) 
         {
-            // If not present, then add it to randomSet
+            // If the number is not in the randomSet array, add it to the randomSet array
             randomSet.push(randomElement);
-        }
-        else if (randomSet.includes(randomElement))
+        } 
+        else // If the number is already in the randomSet array, find another random number
         {
-            // If present, then generate a new randomElement
-            const randomIndex = Math.floor(Math.random() * beginningSet.length);
-            const randomElement = beginningSet[randomIndex];
+            // Find a new random number from the beginningSet (by using a random index) array and add it to the randomSet array
+            const newRandomIndex = Math.floor(Math.random() * beginningSet.length);
+            const newRandomElement = beginningSet[newRandomIndex];
+
+            // Check if the new random number is already in the randomSet array
+            if (!randomSet.includes(newRandomElement)) 
+            {
+                // If the new random number is not in the randomSet array, add it to the randomSet array
+                randomSet.push(newRandomElement);
+            }
         }
     }
-    return randomSet; //Return the new array
-}
 
-//Function to generate a random number between min and max
-function generateRandomNumber(min, max) 
-{
-    //Returns a random integer between min and max
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    // Return the random set of numbers
+    return randomSet;
 }
